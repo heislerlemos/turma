cddcca3cf4c448f27f00dddb73e48256d0b6630b
heisler lemos <heisler.lemos@ita.ao>
2020-04-16 14:25:16 +0100

    test failed

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -4,18 +4,14 @@ class TokensController < ApplicationController
 
   def create
       
-      #account_sid = ENV['TWILIO_ACCOUNT_SID']
-      #api_key = ENV['TWILIO_API_KEY']
-      #api_secret = ENV['TWILIO_API_SECRET']
-        account_sid = ENV['AC4a2579defdde8e8b5bf730e045c37095']
-         api_key = ENV['SK431c2fa98a74b6dd134b44404df8455c']
-         api_secret = ENV['5B8kNbivP3OT2ESUAp6lLPR9Lcjm0uQC']
+      account_sid = ENV['TWILIO_ACCOUNT_SID']
+      api_key = ENV['TWILIO_API_KEY']
+      api_secret = ENV['TWILIO_API_SECRET']
   	identity = current_user.email
 
 
   		grant = Twilio::JWT::AccessToken::ChatGrant.new
-  		#grant.service_sid = ENV['TWILIO_CHAT_SERVICE_SID']
-      grant.service_sid = ENV['IS47d29b41b35b4e00b727e409f8a9e212']
+  		grant.service_sid = ENV['TWILIO_CHAT_SERVICE_SID']
 
   		token = Twilio::JWT::AccessToken.new(
   			account_sid,
